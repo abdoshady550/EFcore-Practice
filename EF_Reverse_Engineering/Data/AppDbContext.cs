551c6946a0c5f4ec2a319fd1b2ecc334cf550e33
Abdelrahman <abdoshady550@gmail.com>
2025-05-23 02:23:47 +0300

    Add EF Core context and entities for events and speakers
    Introduce `AppDbContext` for database management, along with `Event` and `Speaker` entity classes. Configure connection strings from `appsettings.json` and update project file for necessary EF Core packages. Modify `Program` class to display speaker information from the database.

---
+++ EF_Reverse_Engineering/Data/AppDbContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using EF_Reverse_Engineering.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace EF_Reverse_Engineering.Data;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Event> Events { get; set; }
+
+    public virtual DbSet<Speaker> Speakers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+        var constr = configuration.GetSection("constr").Value;
+        optionsBuilder.UseSqlServer(constr);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Event>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Events__3214EC07DD21CA2C");
+
+            entity.HasOne(d => d.Speaker).WithMany(p => p.Events)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Events__SpeakerI__398D8EEE");
+        });
+
+        modelBuilder.Entity<Speaker>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Speakers__3214EC07D133AB65");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ EF_Reverse_Engineering/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using EF_Reverse_Engineering.Data;
+
+internal class Program
+{
+    // Step #1: Package Manager Console (PMC)
+    //    Tools -> Nuget Package Manager -> Package Manager Console
+
+    // Step #2: Package Manager Console (PMC) Tool 
+    //    Install-Package Microsoft.EntityFrameworkCore.Tools
+
+    // Step #3: Install Nuget Page on Project Microsoft.EntityFrameworkCore.Design
+    // Microsoft.EntityFrameworkCore.SqlServer
+
+    // Step #4: Install Provider in the project Microsoft.EntityFrameworkCore.SqlServer
+
+    // Step #5: Run Command (Full)
+    //    Scaffold-DbContext '[Connection String]' [Provider]
+    //PM> Scaffold-DbContext 'Data Source = KAKASHI-PC\SQLEXPRESS ; Initial Catalog =TechTalk ; Integrated Security =SSPI; TrustServerCertificate = True ' Microsoft.EntityFrameworkCore.SqlServer
+    //
+    private static void Main(string[] args)
+    {
+        foreach (var item in new AppDbContext().Speakers)
+        {
+            Console.WriteLine(item.FirstName + " " + item.LastName);
+
+        }
+
+    }
+}
